b42831e77fc81185a561413fac56977a6d8d430a
JCline11 <jasonsmevog@gmail.com>
2026-09-20 01:28:11 -0700

    initial commit

---
+++ Day1.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+
+print(2 + 3) #addition(+)
+print(5 - 2) #subtraction(-)
+print(4 * 2) #multiplication(*)
+print(10 / 2) #division(/)
+print(3 ** 2) #exponentiation(**)
+print(10 % 3) #modulus(%)
+print(10 // 3) #floor division(//)
+
+print(type(10)) #int
+print(type(10.5)) #float
+print(type(3 + 4j)) #complex
+print(type("Hello")) #string
+print(type(True)) #boolean
+print(type([1, 2, 3])) #list
+print(type((1, 2, 3))) #tuple
+print(type({1, 2, 3})) #set
+print(type({"name": "John", "age": 30})) #dictionary
