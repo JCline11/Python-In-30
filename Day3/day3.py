f30a4ca3efb18d062232c56dec9182858dc0911d
JCline11 <jasonsmevog@gmail.com>
2026-09-20 12:22:59 -0700

    started day 3

---
+++ Day3/day3.py
@@ -0,0 +1,28 @@
+# Coding exercise for day 3 of 30
+
+age = 23
+height = 6.1
+complex_num = 4 + 3j
+
+base = int(input("Enter base: "))
+height_triangle = int(input("Enter height: "))
+area = 0.5 * base * height_triangle
+print("The area of the triangle is: ", area)
+
+side_a = int(input("Enter side a: "))
+side_b = int(input("Enter side b: "))
+side_c = int(input("Enter side c: "))
+perimeter = side_a + side_b + side_c
+print("The perimeter of the triangle is: ", perimeter)
+
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+area = length * width
+perimeter = 2 * (length + width)
+print("Area: ", area, "Perimeter: ", perimeter)
+
+radius = int(input("Enter radius: "))
+area = 3.14 * (radius ** 2)
+circumference = 2 * 3.14 * radius
+print("Area: ", area, "Circumference: ", circumference)
+
