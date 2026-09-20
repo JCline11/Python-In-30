730e5fb2e58e5127ee5c8d36eb872b61991b04a1
JCline11 <jasonsmevog@gmail.com>
2026-09-20 01:49:23 -0700

    finished exercise 1 day 2

---
+++ Day2/variables.py
@@ -0,0 +1,13 @@
+# Day 2: 30 Days of Python Programming
+
+firstName = "jason"
+last_name = "Smevog"
+full_name = firstName + " " + last_name
+country = "USA"
+city = "Corona"
+age = 24
+year = 2002
+is_married = False
+is_true = True
+is_light_on = True
+plays_sport, works_out, weighs, height = "golf", True, 198, 6.1
