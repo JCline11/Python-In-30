93492323f531c8e9b3acbf9f2df4042a5da2bca6
jason <jasonsmevog@gmail.com>
2026-09-25 18:11:44 -0700

    completed exercise level 2 for day 6

--- Day6/exercise1.py
+++ Day6/exercise1.py
@@ -7,3 +7,25 @@ siblings = brothers + sisters
 print(len(siblings))
 family_members = siblings + ("Jeff", "Lea")
 print(family_members)
+
+#exercise 2 for day 6 tuples
+
+siblings = family_members[:-2]
+parents = family_members[-2:]
+print(siblings)
+print(parents)
+
+fruits = ['apples', 'oranges', 'lemons']
+vegetables = ['broccoli', 'brussells', 'corn']
+animal_products = ['meat', 'cheese', 'milk']
+food_stuff_tp = fruits + vegetables + animal_products
+food_stuff_lt = list(food_stuff_tp)
+food_stuff_lt = food_stuff_lt[:3] + food_stuff_lt[-3:]
+print(food_stuff_lt)
+food_stuff_lt = food_stuff_lt[3:]
+print(food_stuff_lt)
+del food_stuff_lt
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia: ', 'Estonia' in nordic_countries)
+print('Iceland: ', 'Iceland' in nordic_countries)
