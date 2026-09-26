ae856abe2b7ca0983261a6fa129b8e1b96d368f2
jason <jasonsmevog@gmail.com>
2026-09-25 17:55:08 -0700

    completed exercise 1 day 6

---
+++ Day6/exercise1.py
@@ -0,0 +1,9 @@
+# exercise 1 for day 6 tuples
+
+empty_tuple = ()
+brothers = ("Bob", "Joe")
+sisters = ("Sarah", "Jasmine")
+siblings = brothers + sisters
+print(len(siblings))
+family_members = siblings + ("Jeff", "Lea")
+print(family_members)
